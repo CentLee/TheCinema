99ead6d07761057352214ffe5d00fbc47d2e41d8
SatGatLee <step323@naver.com>
2019-07-08 23:06:08 +0900

    Initial Commit

---
+++ TheCinema/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheCinema
+//
+//  Created by SatGatLee on 2019. 7. 8..
+//  Copyright © 2019년 com.example. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
